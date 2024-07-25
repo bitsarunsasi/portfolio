b58f99a3d2f4eb8ac9087c7e8548ea36a8b6ea70
Arun Sasi <arun.sasi@admaren.com>
2024-07-25 11:28:24 +0530

    init commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,23 @@
+
+
+const menuToggler = document.querySelector('.menu-toggler');
+const menuWrapper = document.querySelector('.navbar__wrapper');
+
+$(menuToggler).on('click', function (event) {
+    $(menuWrapper).toggleClass('navbar__wrapper--active');
+    $(this).toggleClass('menu-toggler--active');
+})
+
+
+// const lenis = new Lenis()
+
+// lenis.on('scroll', (e) => {
+//     console.log(e)
+// })
+
+// function raf(time) {
+//     lenis.raf(time)
+//     requestAnimationFrame(raf)
+// }
+
+// requestAnimationFrame(raf)
